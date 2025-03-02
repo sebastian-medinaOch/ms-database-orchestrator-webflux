c75c2a6a546258f25a631f1f9d131fc442ecd85f
sebastian <sebastian.median@pragma.com.co>
2025-03-02 08:15:16 -0500

    Validación para cuando no exista el registro

--- src/main/java/com/smo/orchestrator/infrastructure/dataproviders/restclients/implementation/ProductRestClient.java
+++ src/main/java/com/smo/orchestrator/infrastructure/dataproviders/restclients/implementation/ProductRestClient.java
@@ -1,11 +1,15 @@
 package com.smo.orchestrator.infrastructure.dataproviders.restclients.implementation;
 
+import com.smo.orchestrator.domain.exceptions.BussinessException;
 import com.smo.orchestrator.domain.ports.on.IGetSimilarProductsIdsUseCaseOn;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
@@ -18,11 +22,17 @@ public class ProductRestClient implements IGetSimilarProductsIdsUseCaseOn {
     public Flux<Integer> getProducts(String productId) {
         return webClientBuilder
                 .get()
-                .uri("/product/{productId}/similarids", productId) // URL con parámetro dinámico
+                .uri("/product/{productId}/similarids", productId)
                 .retrieve()
-                .bodyToFlux(Integer.class) // El servicio devuelve una lista de enteros
+                .onStatus(HttpStatusCode::is4xxClientError, response ->
+                        response.statusCode().equals(HttpStatus.NOT_FOUND)
+                                ? Mono.error(new BussinessException(HttpStatus.NOT_FOUND, "No se encontró ningun regristro bajo ese productId"))
+                                : response.createException().flatMap(Mono::error)
+                )
+                .bodyToFlux(Integer.class)
                 .switchIfEmpty(Flux.empty()); //
     }
+
 
 
    /* private Mono<ProductResponse> getProductDetailId(String productId) {
